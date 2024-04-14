653cebc0cc361c50b8a7015f20cbbbf689639d8b
Swarnim <hofboy04@gmail.com>
2024-04-14 21:41:47 +0530

    incomplete

--- FakeStoreApi/src/main/java/org/example/fakestoreapi/models/Category.java
+++ FakeStoreApi/src/main/java/org/example/fakestoreapi/models/Category.java
@@ -2,10 +2,14 @@ package org.example.fakestoreapi.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Getter
 @Setter
+@Entity
 public class Category {
+    @Id
     private Long id;
     private String title;
     private String description;
